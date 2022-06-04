c2847a75e1e09aa6f020fb9b6729fec48860ee6e
Aaron <addaeaaron28@gmail.com>
2022-06-04 22:25:50 +0000

    update: created local sever and added functionality to site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import "./index.css";
+import Category from "./Category";
+import Menu from "./Menu";
+import items from "./data";
+import { useState } from "react";
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className="menu section">
         <div className="title">
           <h2>Aroma Mocha</h2>
+          <div className="underline"></div>
         </div>
-        <div className="underline"></div>
+        <Category categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
